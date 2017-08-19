1083ee194c6abef8a30e92c3882cecbf1a9ece33
meghamca16du <megha.mca16.du@gmail.com>
2017-08-19 21:11:39 +0530

    mentors page upload

--- src/pages/mentors/mentors.ts
+++ src/pages/mentors/mentors.ts
@@ -1,20 +1,47 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { LoginPage } from '../login/login';
+import { UsersServiceProvider } from '../../providers/users-service/users-service';
+import * as firebase from 'firebase';
+import { AlertController } from 'ionic-angular';
+import {Injectable} from '@angular/core';
 
-/**
- * Generated class for the MentorsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-mentors',
   templateUrl: 'mentors.html',
+  providers: [UsersServiceProvider]
 })
 export class MentorsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private db1: any;
+  private delegatepg1: any;
+  private contarr=[];
+  private linkarr=[];
+  private namearr=[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private userservice:UsersServiceProvider,public alertCtrl: AlertController) {
+
+    this.db1 = firebase.database().ref('/'); // Get a firebase reference to the root
+      this.delegatepg1 = firebase.database().ref('mentors'); // Get a firebase reference to the homepage
+    
+  var that= this;
+    this.delegatepg1.on("value",function(snappy){
+      var i=0;
+    snappy.forEach(function(snap){
+       
+        var links = snap.child('link');
+        var content = snap.child('content');
+        var name = snap.child('name');
+        that.linkarr[i] = links.val();
+        that.contarr[i] = content.val();
+        that.namearr[i] = name.val();
+        i=i+1;
+
+      }); 
+      });
+
   }
 
   ionViewDidLoad() {
