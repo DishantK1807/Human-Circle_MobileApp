f0c250ccf2e4656454c23ec728f9b7e9ea852f69
meghamca16du <megha.mca16.du@gmail.com>
2017-08-19 21:18:40 +0530

    opportunities page upload

--- src/pages/opportunities/opportunities.ts
+++ src/pages/opportunities/opportunities.ts
@@ -1,31 +1,51 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Http } from '@angular/http';
+import { LoginPage } from '../login/login';
+import { UsersServiceProvider } from '../../providers/users-service/users-service';
 import * as firebase from 'firebase';
+import { AlertController } from 'ionic-angular';
+import {Injectable} from '@angular/core';
 
-/**
- * Generated class for the OpportunitiesPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-opportunities',
   templateUrl: 'opportunities.html',
+  providers: [UsersServiceProvider]
 })
 export class OpportunitiesPage {
-//public url:any;
-//public fbdata:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  //this.fbdata=firebase.database().ref('links');
+
+  private db1: any;
+  private delegatepg1: any;
+  private contarr=[];
+  private linkarr=[];
+  private headarr=[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private userservice:UsersServiceProvider,public alertCtrl: AlertController) {
+
+    this.db1 = firebase.database().ref('/'); // Get a firebase reference to the root
+      this.delegatepg1 = firebase.database().ref('opportunities'); // Get a firebase reference to the homepage
+    
+  var that= this;
+    this.delegatepg1.on("value",function(snappy){
+      var i=0;
+    snappy.forEach(function(snap){
+       
+        var links = snap.child('link');
+        var content = snap.child('content');
+        var head = snap.child('heading');
+        that.linkarr[i] = links.val();
+        that.contarr[i] = content.val();
+        that.headarr[i] = head.val();
+        i=i+1;
+
+      }); 
+      });
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OpportunitiesPage');
   }
- // addlinks(){
- // this.fbdata.set({link:this.url.trim()});
- // alert("link added:"+this.url.trim());
- // }
+
 }
