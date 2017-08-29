bf8c0134f81cad94331a3ae857135907096124f0
meghamca16du <megha.mca16.du@gmail.com>
2017-08-29 20:27:11 +0530

    personalinfo page layout done

--- src/pages/teamdetails/teamdetails.ts
+++ src/pages/teamdetails/teamdetails.ts
@@ -25,9 +25,14 @@ export class TeamdetailsPage {
   private mentors=[];
   private members=[];
   private flag:any;
-  private mname:any;
-  private mcontact:any;
-  private mlink:any;
+
+  //variables for displaying info
+  private userFname=[];
+  private userLname=[];
+  private userEmail=[];
+
+  private userNumber=[];
+  
 
   constructor(private platform: Platform, public navCtrl: NavController,private geolocation: Geolocation, public navParams: NavParams,private userservice:UsersServiceProvider,public alertCtrl: AlertController) {
   var myUserId = firebase.auth().currentUser.uid; //current user id
@@ -35,14 +40,16 @@ export class TeamdetailsPage {
     this.db = firebase.database().ref('/'); // Get a firebase reference to the root
       this.homepg = firebase.database().ref('teamdetails'); // Get a firebase reference to the homepage
     
-  var that= this;
+  var that= this; that.flag=0;
     this.homepg.on("value",function(snappy){
       var i=0;
     snappy.forEach(function(snapi){
-       var lmentor = snapi.child('mentor');
-       
+      console.log('my user id is:',myUserId);
+      if (that.flag==1)
+        {return;}
+       var lmentor = snapi.child('mentor');      
        var lteam = snapi.child('team');
-         var j=0; that.flag=0;
+         var j=0; 
           lteam.forEach(function(m){
               that.members[j]=m.val();
                   if (m.val()==myUserId)
@@ -56,6 +63,7 @@ export class TeamdetailsPage {
             { var t=0;
               lmentor.forEach(function(n){
                 that.mentors[t] = n.val();
+                console.log(n.val());
                 t=t+1;
               });
             }
@@ -64,10 +72,26 @@ export class TeamdetailsPage {
             { var k=0; 
               while(j>=1)
                 {console.log(that.members[k]);
-                  
-              k=k+1; j=j-1;}
-              return;
-            }
+                                    
+                          
+                          that.userservice.viewUser(that.members[k]).then(snapshot => {
+                          console.log('keyyyyyyyyyy',snapshot.key);
+                          console.log('firsttttttttt',snapshot.val().firstname);
+                          console.log(snapshot.val().lastname);
+                          console.log(snapshot.val().email);
+                          console.log(snapshot.val().number);
+                            that.userFname[k] = snapshot.val().firstname;
+                            
+                            that.userLname[k] = snapshot.val().lastname;
+                            that.userEmail[k] = snapshot.val().email;
+                            //then.userSex = snapshot.val().sex;
+                            that.userNumber[k] = snapshot.val().number;
+                            //then.userUsername = snapshot.val().username;
+                          })
+
+                k=k+1; j=j-1;}
+                that.check();
+                }
       
                    
           /*  if (m.val()==myUserId)
@@ -79,10 +103,12 @@ export class TeamdetailsPage {
         i=i+1;
 
       }); 
-      //});
-   // });
     
      }  
+ check(){
+   console.log('i should END');
+ }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TeamdetailsPage');
